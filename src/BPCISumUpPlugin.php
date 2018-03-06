2008cfdf19ae65f587a30e39027a898d5947dc27
Bruce Pinheiro de Carvalho <brucepcarvalho@gmail.com>
2018-03-06 13:38:06 -0400

    Initial commit

---
+++ src/BPCISumUpPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BPCI\SumUpPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class BPCISumUpPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
